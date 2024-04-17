e69673614d2da325b4b9c38ab311878e29fde15b
Wanza Nashami <wanzanashami@gmail.com>
2024-04-17 13:18:44 +0300

    unsuccessfully tries to reassign the least favorite customer

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+// Declare a variable in global scope called customerName
+var customerName = 'bob';
+
+// Write a function that accesses the global customerName variable, and uppercases it
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Write a new function called overwriteBestCustomer() that changes that bestCustomer variable to 'maybe bob'
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope called leastFavoriteCustomer and assign it some initial value
+const leastFavoriteCustomer = 'someone';
+
+// Write a function called changeLeastFavoriteCustomer() that attempts to change that constant
+function changeLeastFavoriteCustomer() {
+  // JavaScript won't allow reassignment of a constant, so this line will throw an error
+  leastFavoriteCustomer = 'someone else';
+}
